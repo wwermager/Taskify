fb6ab5daa8fa55dbc6dffd4089ba3170c0b7403b
wwermager <wwermager09@winona.edu>
2017-10-31 22:43:44 -0500

    upload
    Signed-off-by: wwermager <wwermager09@winona.edu>

--- Taskify/ViewController.swift
+++ Taskify/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
