a55d9aa85c3768c14ac51138170677e2dd32a9d2
wwermager <wwermager09@winona.edu>
2017-11-24 15:52:16 -0600

    added a text field to center of AddTask view

--- Taskify/AddTask.swift
+++ Taskify/AddTask.swift
@@ -15,21 +15,45 @@ class AddTask: UIViewController {
     let doneBarButton: UIBarButtonItem
     var nav: UINavigationItem
     let navBar: UINavigationBar
+    let taskTitle: UITextField
     
     
     init() {
+        // Navigation Bar
         navBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: (UIScreen.main.bounds.height)/9))
         nav = UINavigationItem(title: "New Task")
         backBarButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: nil, action: #selector(cancelTapped))
         doneBarButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(doneTapped))
         
+        // Input field declarations
+        taskTitle = UITextField()
+        
         super.init(nibName: nil, bundle: nil)
+        
+        // Navigation Bar Buttons
         nav.leftBarButtonItem = backBarButton
         nav.rightBarButtonItem = doneBarButton
         navBar.setItems([nav], animated: false)
 
         self.view = UIView(frame: CGRect(x: 0, y: PHONEHEIGHT/9, width: PHONEWIDTH, height: PHONEHEIGHT-PHONEHEIGHT/9))
+        
+        // Input Field Layouts
+        taskTitle.frame = CGRect(x: (PHONEWIDTH/2)-100, y: PHONEHEIGHT/2, width: 200, height: 50)
+        taskTitle.textColor = UIColor.black
+        taskTitle.font = UIFont.systemFont(ofSize: 17.0)
+        taskTitle.placeholder = "<enter new top label>"
+        taskTitle.backgroundColor = UIColor.white
+        taskTitle.borderStyle = UITextBorderStyle.bezel
+        taskTitle.keyboardType = UIKeyboardType.default
+        taskTitle.returnKeyType = UIReturnKeyType.done
+        taskTitle.clearButtonMode = UITextFieldViewMode.always
+//        taskTitle.delegate = self
+        // Add objects to view
+        self.view.addSubview(taskTitle)
         self.view.addSubview(navBar)
+        
+//        Need to add text fields to subview
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
