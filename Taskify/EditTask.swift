71bf23a23322bca46528e734aeeaa6a9b4d4ef4d
wwermager <wwermager09@winona.edu>
2017-11-28 20:58:12 -0600

    added fields to EditTask

--- Taskify/EditTask.swift
+++ Taskify/EditTask.swift
@@ -10,7 +10,15 @@ import UIKit
 
 class EditTask: AddTask {
     
+//    let taskTitle: UITextField
+//    let titleLabel: UILabel
+//    let taskNote: UITextView // need text view to get multiline
+//    let noteLabel: UILabel
+//    let dueDate: UIDatePicker
+//    let dateLabel: UILabel
+    
     override init() {
+        
         super.init()
         
         nav = UINavigationItem(title: "Edit Task")
@@ -19,6 +27,12 @@ class EditTask: AddTask {
         navBar.setItems([nav], animated: false)
         
         self.view = UIView(frame: CGRect(x: 0, y: PHONEHEIGHT/9, width: PHONEWIDTH, height: PHONEHEIGHT-PHONEHEIGHT/9))
+        self.view.addSubview(taskTitle)
+        self.view.addSubview(titleLabel)
+        self.view.addSubview(taskNote)
+        self.view.addSubview(dueDate)
+        self.view.addSubview(dateLabel)
+        self.view.addSubview(noteLabel)
         self.view.addSubview(navBar)
  
     }
